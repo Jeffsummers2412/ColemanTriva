06b4dff4c2a4cc94bca0f9f47fb3bbd0fa59c6d1
660252823 <jeffsummers2412@yahoo.com>
2017-12-15 11:02:42 -0800

    Initial commit

---
+++ app/src/main/java/summers/colemantriva/MainActivity.java
@@ -0,0 +1,232 @@
+package summers.colemantriva;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Color;
+import android.support.constraint.ConstraintLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.sql.BatchUpdateException;
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    //define varibales
+    private Button answer1, answer2, answer3, answer4;
+    private TextView score;
+    private TextView question;
+
+    private Questions mQuestions = new Questions();
+    private String mAnswer;
+    private int mScore = 0;
+    private int mQuestionLength = mQuestions.sQutestions.length;
+
+    Random random;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar snackbar = Snackbar
+                        .make(view, "Unable to send Email. No Defalut Email Client Installed", Snackbar.LENGTH_LONG)
+                        .setAction("Retry", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Toast.makeText(getApplicationContext(), "Retry is clicked", Toast.LENGTH_LONG).show();
+                            }
+                        });
+
+                snackbar.setActionTextColor(Color.RED);
+                View snackbarView = snackbar.getView();
+                snackbarView.setBackgroundColor(Color.DKGRAY);
+
+                TextView textView = snackbarView.findViewById(android.support.design.R.id.snackbar_text);
+                textView.setTextColor(Color.YELLOW);
+                snackbar.show();
+
+            }
+        });
+
+
+        random = new Random();
+
+        //get reference to the widgets
+        answer1 = (Button) findViewById(R.id.answer1);
+        answer2 = (Button) findViewById(R.id.answer2);
+        answer3 = (Button) findViewById(R.id.answer3);
+        answer4 = (Button) findViewById(R.id.answer4);
+        score = (TextView) findViewById(R.id.score);
+        question = (TextView) findViewById(R.id.question);
+
+        updateQuestions(random.nextInt(mQuestionLength));
+
+        answer1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(answer1.getText() == mAnswer){
+                    mScore++;
+                    score.setText("Score: " + mScore);
+                    updateQuestions(random.nextInt(mQuestionLength));
+                }else{
+                    gameOver();
+                }
+            }
+        });
+        answer2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(answer2.getText() == mAnswer){
+                    mScore++;
+                    score.setText("Score: " + mScore);
+                    updateQuestions(random.nextInt(mQuestionLength));
+                }else{
+                    gameOver();
+                }
+            }
+        });
+        answer3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(answer3.getText() == mAnswer){
+                    mScore++;
+                    score.setText("Score: " + mScore);
+                    updateQuestions(random.nextInt(mQuestionLength));
+                }else{
+                    gameOver();
+                }
+            }
+        });
+        answer4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(answer4.getText() == mAnswer){
+                    mScore++;
+                    score.setText("Score: " + mScore);
+                    updateQuestions(random.nextInt(mQuestionLength));
+                }else{
+                    gameOver();
+                }
+            }
+        });
+
+
+    }
+
+
+    private void updateQuestions(int num){
+        question.setText(mQuestions.getQuestion(num));
+        answer1.setText(mQuestions.getChoice1(num));
+        answer2.setText(mQuestions.getChoice2(num));
+        answer3.setText(mQuestions.getChoice3(num));
+        answer4.setText(mQuestions.getChoice4(num));
+
+        mAnswer = mQuestions.getAnswer(num);
+
+    }
+    private void gameOver() {
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(MainActivity.this);
+        alertDialogBuilder
+                .setMessage("Game Over Your Score is " + mScore + "points")
+                .setCancelable(false)
+                .setPositiveButton("NEW GAME",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                            }
+                        })
+                .setNegativeButton("EXIT",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                finish();
+                            }
+                        });
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+       RelativeLayout main = (RelativeLayout) findViewById(R.id.main);
+
+        switch (item.getItemId()) {
+
+            case R.id.menu_red:
+                if(item.isChecked()){
+                    item.setChecked(false);
+                }
+                else{
+                    item.setChecked(true);
+                }
+
+                main.setBackgroundColor(Color.rgb(215, 60, 60));
+                return true;
+            case R.id.menu_green:
+                if(item.isChecked()){
+                    item.setChecked(false);
+                }
+                else{
+                    item.setChecked(true);
+                }
+
+                main.setBackgroundColor(Color.rgb(150, 255, 140));
+                return true;
+            case R.id.menu_blue:
+                if(item.isChecked()){
+                    item.setChecked(false);
+                }
+                else{
+                    item.setChecked(true);
+                }
+
+                main.setBackgroundColor(Color.rgb(115, 145, 255));
+                return true;
+            case R.id.menu_yellow:
+                if(item.isChecked()){
+                    item.setChecked(false);
+                }
+                else{
+                    item.setChecked(true);
+                }
+
+                main.setBackgroundColor(Color.rgb(255, 255, 0));
+                return true;
+            case R.id.action_settings:
+                Intent i = new Intent(this, MyPreferencesActivity.class);
+                startActivity(i);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+    }
+}
---
+++ app/src/main/java/summers/colemantriva/Questions.java
@@ -0,0 +1,86 @@
+package summers.colemantriva;
+
+/**
+ * Created by 660252823 on 12/14/2017.
+ */
+
+public class Questions {
+
+    public String sQutestions[] = {
+            "In what year was Coleman University Founded?",
+            "What is the name of the Founder of Coleman University?",
+            "What is Coleman University Slogen?",
+            "What was the nickname of the first Coleman University facility called?",
+            "In 2016 what was the name Coleman University change the Network Security Program to?",
+            "How many years has Coleman University been serving the community of San Diego?",
+            "What is Coleman University Street Address?",
+            "How old is Charlie Morgan?",
+            "In what year did Leticia Rabor join the Coleman University staff?",
+            "How is the Best instructor at Coleman?",
+
+    };
+
+    public String sAnswers[][]= {
+            {"1971","1963","1977","1961"},
+            {"Dr. Coleman Furr","Dr. Coleman Burr","Dr. Charlie Morgan","Dr. Jeff Summers"},
+            {"Technology Focused Careers","Experience Technology","Dreams into Reality","No More Homework!"},
+            {"The Bowling Alley","Automation Institute of San Diego","Shake Shack","The Pit"},
+            {"Internet Detectives","Firewall Professionals","Internet Security","Cyber Security"},
+            {"45","50","71","52"},
+            {"8891 Balboa Ave.","1134 Coleman Dr.","8888 Balboa Ave.","4 Private Dr."},
+            {"75","91","71","27"},
+            {"2013","2012","1989","1776"},
+            {"Dr. Coleman Furr","Charlie Morgan","Laticia Rabor","Tommy Bahama"},
+
+    };
+
+    public String sCorrectAnswers[] = {
+            "1963",
+            "Dr. Coleman Furr",
+            "Dreams into Reality",
+            "The Bowling Alley",
+            "Cyber Security",
+            "50",
+            "8888 Balboa Ave.",
+            "75",
+            "2012",
+            "Laticia Rabor",
+    };
+
+    public String getQuestion(int a){
+        String question = sQutestions[a];
+        return question;
+    }
+
+    public String getChoice1(int a){
+        String choice1 = sAnswers[a][0];
+        return choice1;
+
+    }
+
+    public String getChoice2(int a){
+        String choice1 = sAnswers[a][1];
+        return choice1;
+
+    }
+
+    public String getChoice3(int a){
+        String choice1 = sAnswers[a][2];
+        return choice1;
+
+    }
+
+    public String getChoice4(int a){
+        String choice1 = sAnswers[a][3];
+        return choice1;
+
+    }
+
+    public String getAnswer(int a){
+        String answer = sCorrectAnswers[a];
+        return answer;
+
+
+    }
+
+}
